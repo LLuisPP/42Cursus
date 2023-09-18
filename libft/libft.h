f7e5e0f987ae8b6d0ce099e9e861f18fd4e18bc8
LLuisPP <116104082+LLuisPP@users.noreply.github.com>
2023-09-18 20:03:48 +0200

    makefile y libft.h actualizados hasta memset y bzero

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: lprieto- <lprieto-@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/15 19:14:45 by lprieto-          #+#    #+#             */
-/*   Updated: 2023/09/18 10:16:35 by lprieto-         ###   ########.fr       */
+/*   Updated: 2023/09/18 19:15:38 by lprieto-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,18 @@
 # include <ctype.h>
 # include <stdio.h>
 
-int		ft_strlen(const char *s);
-int		ft_isalnum(int c);
-int		ft_isalpha(int c);
-int		ft_isascii(int c);
-int		ft_isdigit(int c);
-int		ft_isprint(int c);
-int		ft_tolower(int c);
-int		ft_toupper(int c);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int	ft_strlen(const char *s);
+int	ft_isalnum(int c);
+int	ft_isalpha(int c);
+int	ft_isascii(int c);
+int	ft_isdigit(int c);
+int	ft_isprint(int c);
+int	ft_tolower(int c);
+int	ft_toupper(int c);
+int	ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strchr(const char *s, int c);
+void	*ft_memset(void *b, int c, size_t len);
 char	*ft_strdup(char *str);
 char	*ft_strrchr(const char *s, int c);
+void	ft_bzero(void *s, size_t n);
 #endif
