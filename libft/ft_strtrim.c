079f9bcda5dee42fb074dd90aecfc6f302b34cb3
LLuisPP <116104082+LLuisPP@users.noreply.github.com>
2023-10-02 14:01:18 +0200

    strtrim

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,73 +6,91 @@
 /*   By: lprieto- <lprieto-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/30 06:33:40 by lprieto-          #+#    #+#             */
-/*   Updated: 2023/09/30 13:03:30 by lprieto-         ###   ########.fr       */
+/*   Updated: 2023/10/02 14:00:55 by lprieto-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strtrim(char const *s1, char const *set)
+int	static	fend(char const *s1, char const *set)
 {
-	int		i;
-	int		j;
-	int		start;
-	int		end;
-	int		s;
-	char	*ret;
+	int	i;
+	int	j;
+	int	p;
 
 	i = 0;
 	j = 0;
-	if (!s1 || !set)
-		return (0);
-	while (set[j] != '\0')
+	while (set[j] != 0)
 		j++;
-	s = j;
-	while (s1[i] != '\0')
-	{                    
-		if (s1[i] != set[j])
-		{
-			while (j >= 0)
-			{
-				if (s1[i] == set[j])
-				{
-					end = i;
-					break ;
-				}
-				j--;
-			}
-		}
-		j = s;
-		i++;
-	}
-	while (s1[i] != '\0')
+	p = j;
+	while (s1[i] != 0)
 		i++;
 	while (i >= 0)
 	{
-		if (s1[i] != set[j])
+		j = p;
+		while (j >= 0)
 		{
-			while (j >= s)
-			{
-				if (s1[i] == set[j])
-				{
-					start = i;
-					break ;
-				}
-			}
-			j--;
+			if (s1[i] != set[j])
+				return (i);
+			else
+				j--;
 		}
-		j = s;
-		i++;
+		i--;
 	}
-	ret = (char *)malloc(sizeof(char) * (200));
-	if (!ret)
-		return ((char *) 0);
-	i = 0;
-	while (i <= end - start)
+	return (i);
+}
+
+int static	fstart(char const *s1, char const *set)
+{
+	int	h;
+	int	k;
+/*	int	p*/
+
+	h = 0;
+	k = 0;
+/*	while (set[k] != '\0')
+		k++;
+	p = k;
+		printf("K fuera del while: %d \n", k);*/
+	while (s1[h] != '\0')
 	{
-		ret[i] = s1[start];
+		/*k = p;
+		printf("K reiniciado a P: %d \n", k);*/
+		while (set[k] != '\0')
+		{
+			if (s1[h] == set[k])
+			h++;
+			if (s1[h] != set[k])
+					k++;
+			}
+		return (h);
+	}
+		printf("en K: %d \n", k);
+	return (h);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	int		c;
+	int		i;
+	int		f;
+	char	*ret;
+
+	c = 0;
+	f = fend(s1, set);
+	printf("final: %d \n", f);
+	i = fstart(s1, set);
+	printf("inici: %d \n", i);
+	if (!s1 || !set)
+		return (0);
+	ret = (char *)malloc(sizeof(char) * (f - i) + 1);
+	if (!ret)
+		return (NULL);
+	while (c <= (f - i) + 1)
+	{
+		ret[c] = s1[i];
+		c++;
 		i++;
-		start++;
 	}
 	return (ret);
 }
@@ -81,7 +99,6 @@ int	main(int argc, char **argv)
 {
 	if (argc != 3)
 		return (0);
-	ft_strtrim(argv[1], argv[2]);
-	printf("ft_strtim: %s \n", ft_strtrim(argv[1], argv[2]));
+	printf("ft_strtim array output: %s \n", ft_strtrim(argv[1], argv[2]));
 	return (0);
 }
