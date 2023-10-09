f16680e503a5ac498c98d2b9185de594e47677d1
LLuisPP <116104082+LLuisPP@users.noreply.github.com>
2023-10-09 13:44:41 +0200

    Add files via upload

--- libft/ft_strmapi.c
+++ libft/ft_strmapi.c
@@ -6,13 +6,23 @@
 /*   By: lprieto- <lprieto-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/06 10:57:32 by lprieto-          #+#    #+#             */
-/*   Updated: 2023/10/06 10:59:50 by lprieto-         ###   ########.fr       */
+/*   Updated: 2023/10/09 10:41:08 by lprieto-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strmapi(char const *s, char (*f)(unsigned in, char))
+static int	stringlen(char const	*s)
+{
+	int	slen;
+
+	slen = 0;
+	while (s[slen] != 0)
+		slen++;
+	return (slen);
+}
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
 	unsigned int	i;
 	char			*str;
@@ -20,5 +30,34 @@ char	*ft_strmapi(char const *s, char (*f)(unsigned in, char))
 	if (!s)
 		return (NULL);
 	i = 0;
-	str = (char)malloc(sizeof(char)
+	str = (char *)malloc(sizeof(char) * (stringlen(s) + 1));
+	if (str == NULL)
+		return (NULL);
+	while (s[i] != '\0')
+	{
+		str[i] = f(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
 }
+
+/*char	toupper(unsigned int i, char c)
+{
+	(void)i;
+	if (c >= 'a' && c <= 'z')
+		return (c - 32);
+	return (c);
+}
+
+int	main(void)
+{
+	char	*str;
+	char	*result;
+
+	str = "Hola Mundo!";
+	result = ft_strmapi(str, &toupper);
+	ft_putstr_fd(result, 1);
+	free(result);
+	return (0);
+}*/
