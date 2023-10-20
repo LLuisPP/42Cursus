5c4fe1abc79c18f4d9bc57899e86a3e35912825c
LLuisPP <116104082+LLuisPP@users.noreply.github.com>
2023-10-20 12:18:19 +0200

    Add files via upload

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,26 +6,21 @@
 /*   By: lprieto- <lprieto-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 10:28:39 by lprieto-          #+#    #+#             */
-/*   Updated: 2023/10/20 11:13:44 by lprieto-         ###   ########.fr       */
+/*   Updated: 2023/10/20 12:18:06 by lprieto-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libftprintf.h"
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdarg.h>
-#include <stdio.h>
+#include "ft_printf.h"
 
 static void	ft_putchar(char c)
 {
 	write(1, &c, 1);
-
 }
 
 static void	ft_putstr(char *s)
 {
 	int	len;
-	int i;
+	int	i;
 
 	i = 0;
 	if (s)
@@ -42,41 +37,32 @@ static void	ft_putstr(char *s)
 	}
 }
 
-static 
+static void	ft_ptr(char *p)
+{
+	ft_putstr("0x");
+	ft_putstr(
+}
 
 static int	ft_next_pholder(char const *str, va_list args, int i, int j)
 {
-	if (str[j] == 'c') //char
+	if (str[j] == 'c')
 		ft_putchar(va_arg(args, int));
-	if (str[j] == 's')	//string
+	if (str[j] == 's')
 		ft_putstr(va_arg(args, char *));
-/*	if (str[j] == 'p')	//puntero
-		write (1, 'p', 1);
-	if (str[j] == 'd')	//decimal
-		write (1, 'd', 1);
-	if (str[j] == 'i')	//entero base 10
-		write (1, 'i', 1);
-	if (str[j] == 'u')	//unsigned decimal
-		write (1, 'u', 1);
-	if (str[j] == 'x')	// hexadecimal minusculas
-		write (1, 'x', 1);
-	if (str[j] == 'X')	//hexadecimal MAYUSCULAS
-		write (1, 'X', 1);
-	if (str[j] == '%')	//simbolo de %
-		write (1, '%', 1);*/
+	if (str[j] == 'p')
+		ft_ptr(va_arg(args, char *));
 	return (i);
 }
 
-int ft_printf(char const *str, ...)
+int	ft_printf(char const *str, ...)
 {
-	va_list args;
-	int	i;
-	int	j;
+	va_list	args;
+	int		i;
+	int		j;
 
 	i = 0;
 	j = 0;
 	va_start(args, str);
-
 	while (str[j])
 	{
 		if (str[j] == '%')
@@ -95,14 +81,26 @@ int ft_printf(char const *str, ...)
 	return (i);
 }
 
-int	main()
+/*int	main()
 {
 	ft_printf("MY -->hola mon! c: %c, s: %s\n", 'a', "hola");
 	printf("-------------------\n");
 	printf("OG -->hola mon! c: %c, s: %s\n", 'a', "hola");
 	return (0);
-}
-
-
-
-/*	ft_printf("MY -->hola mon! c: %c, s: %s, p: %p, d: %d, i: %i, u: %u, x: %x, X: %X, %: %%", "7", "hola", 0.25, 10, 0.30, '61', '41', "%"  \n); */
+}*/
+/*	if (str[j] == 'p')	//puntero
+		write (1, 'p', 1);
+	if (str[j] == 'd')	//decimal
+		write (1, 'd', 1);
+	if (str[j] == 'i')	//entero base 10
+		write (1, 'i', 1);
+	if (str[j] == 'u')	//unsigned decimal
+		write (1, 'u', 1);
+	if (str[j] == 'x')	// hexadecimal minusculas
+		write (1, 'x', 1);
+	if (str[j] == 'X')	//hexadecimal MAYUSCULAS
+		write (1, 'X', 1);
+	if (str[j] == '%')	//simbolo de %
+		write (1, '%', 1);*/
+/*	ft_printf("MY -->hola mon! c: %c, s: %s, p: %p, d: %d, i: %i, u: %u, 
+ *	x: %x, X: %X, %: %%", "7", "hola", 0.25, 10, 0.30, '61', '41', "%"  \n); */
---
+++ ft_printf/ft_printf.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lprieto- <lprieto-@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/12 10:54:43 by lprieto-          #+#    #+#             */
+/*   Updated: 2023/10/20 12:06:26 by lprieto-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdarg.h>
+# include <stdio.h>
+
+int	ft_printf(char const *str, ...);
+
+#endif
