3d6b9036a96fc2206e31b6448e14abbb243593f9
LLuisPP <116104082+LLuisPP@users.noreply.github.com>
2023-10-20 11:21:34 +0200

    Add files via upload

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -6,19 +6,103 @@
 /*   By: lprieto- <lprieto-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 10:28:39 by lprieto-          #+#    #+#             */
-/*   Updated: 2023/10/12 10:39:44 by lprieto-         ###   ########.fr       */
+/*   Updated: 2023/10/20 11:13:44 by lprieto-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdio.h>
 
-int ft_printf(char const *, ...)
+static void	ft_putchar(char c)
 {
+	write(1, &c, 1);
+
 }
 
-int	main(int argc, char **argv)
+static void	ft_putstr(char *s)
 {
-	
+	int	len;
+	int i;
 
-
+	i = 0;
+	if (s)
+	{
+		while (s[i] != 0)
+			i++;
+		len = i;
+		i = 0;
+		while (s[i] != 0)
+		{
+			ft_putchar(s[i]);
+			i++;
+		}
+	}
 }
+
+static 
+
+static int	ft_next_pholder(char const *str, va_list args, int i, int j)
+{
+	if (str[j] == 'c') //char
+		ft_putchar(va_arg(args, int));
+	if (str[j] == 's')	//string
+		ft_putstr(va_arg(args, char *));
+/*	if (str[j] == 'p')	//puntero
+		write (1, 'p', 1);
+	if (str[j] == 'd')	//decimal
+		write (1, 'd', 1);
+	if (str[j] == 'i')	//entero base 10
+		write (1, 'i', 1);
+	if (str[j] == 'u')	//unsigned decimal
+		write (1, 'u', 1);
+	if (str[j] == 'x')	// hexadecimal minusculas
+		write (1, 'x', 1);
+	if (str[j] == 'X')	//hexadecimal MAYUSCULAS
+		write (1, 'X', 1);
+	if (str[j] == '%')	//simbolo de %
+		write (1, '%', 1);*/
+	return (i);
+}
+
+int ft_printf(char const *str, ...)
+{
+	va_list args;
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	va_start(args, str);
+
+	while (str[j])
+	{
+		if (str[j] == '%')
+		{
+			ft_next_pholder(str, args, i, ++j);
+			j++;
+		}
+		else
+		{
+			i++;
+			write(1, &str[j], 1);
+			j++;
+		}
+	}
+	va_end(args);
+	return (i);
+}
+
+int	main()
+{
+	ft_printf("MY -->hola mon! c: %c, s: %s\n", 'a', "hola");
+	printf("-------------------\n");
+	printf("OG -->hola mon! c: %c, s: %s\n", 'a', "hola");
+	return (0);
+}
+
+
+
+/*	ft_printf("MY -->hola mon! c: %c, s: %s, p: %p, d: %d, i: %i, u: %u, x: %x, X: %X, %: %%", "7", "hola", 0.25, 10, 0.30, '61', '41', "%"  \n); */
