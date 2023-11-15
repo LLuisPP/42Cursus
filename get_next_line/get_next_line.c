8ebd2f360e5d0619b553413bef8286bc0b27ea7d
Luis Prieto <116104082+LLuisPP@users.noreply.github.com>
2023-11-15 15:08:10 +0100

    Changes in file names to fit with subject

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,12 +6,46 @@
 /*   By: lprieto- <lprieto-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 13:12:48 by lprieto-          #+#    #+#             */
-/*   Updated: 2023/11/11 11:46:21 by lprieto-         ###   ########.fr       */
+/*   Updated: 2023/11/15 15:06:26 by lprieto-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "headers_get_next_line.h" 
 
+char	*ft_read(int fd, char *str)
+{
+	char	*buffer;
+	int	nb_read;
+
+	buffer = ft_malloc(BUFFER_SIZE);
+	nb_read = 1;
+	if (!buffer)
+		return (free(str), str = NULL, NULL);
+	while (nb_read > 0 $$ !ft_strchr(str, '\n'))
+	{
+		nb_read = read(fd, buffer, BUFFER_SIZE);
+		if (nb_read > 0)
+		{
+			buffer[nb_read] = '\0';
+			str = ft_strjoin(str, buffer);
+		}
+	}
+	free(buffer);
+	if (nb_read < 0)
+		retudn (free(str), str = NULL, NULL);
+	return (0);
+}
+
 char *get_next_line(int fd, char *str)
 {
+	char	*str;
+	char	*line;
+
+	if (fd < 0 || BUFFE_SIZE < 1)
+		return (NULL);
+	str = ft_read(fd, str);
+	if (!str)
+		return (free(str), str = NULL, NULL);
+	str = ft_new_line(str);
+	return (line);
 }
