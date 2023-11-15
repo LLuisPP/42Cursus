d8bb649716fc4a1336786d46abef097679e7b04b
Luis Prieto <116104082+LLuisPP@users.noreply.github.com>
2023-11-15 17:32:02 +0100

    Add files via upload

--- get_next_line/get_next_line/get_next_line.c
+++ get_next_line/get_next_line/get_next_line.c
@@ -3,25 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lprieto- <lprieto-@student.42barcel>       +#+  +:+       +#+        */
+/*   By: lprieto- <lprieto-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 13:12:48 by lprieto-          #+#    #+#             */
-/*   Updated: 2023/11/14 21:49:11 by lprieto-         ###   ########.fr       */
+/*   Updated: 2023/11/15 16:10:44 by lprieto-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "headers_get_next_line.h" 
+# include "get_next_line.h"
 
 char	*ft_read(int fd, char *str)
 {
 	char	*buffer;
 	int	nb_read;
 
-	buffer = ft_malloc(BUFFER_SIZE);
+	buffer = ft_malloc(BUFFER_SIZE, 0);
 	nb_read = 1;
 	if (!buffer)
 		return (free(str), str = NULL, NULL);
-	while (nb_read > 0 $$ !ft_strchr(str, '\n'))
+	while (nb_read > 0 && !ft_strchr(str, '\n'))
 	{
 		nb_read = read(fd, buffer, BUFFER_SIZE);
 		if (nb_read > 0)
@@ -32,21 +32,29 @@ char	*ft_read(int fd, char *str)
 	}
 	free(buffer);
 	if (nb_read < 0)
-		retudn (free(str), str = NULL, NULL);
+		return (free(str), str = NULL, NULL);
 	return (0);
 }
 
-
-char	*get_next_line(int fd)
+char *get_next_line(int fd)
 {
 	char	*str;
 	char	*line;
 
-	if (fd < 0 || BUFFE_SIZE < 1)
+	if (fd < 0 || BUFFER_SIZE < 1)
 		return (NULL);
 	str = ft_read(fd, str);
 	if (!str)
 		return (free(str), str = NULL, NULL);
 	str = ft_new_line(str);
 	return (line);
+}
+
+int main (void)
+{
+ int fd = open("punkmanifesto.txt", O_RDONLY);
+ char *line;
+ while ((line = get_next_line(fd)) != NULL)
+     printf("%s", line);
+ return (0);
 }
