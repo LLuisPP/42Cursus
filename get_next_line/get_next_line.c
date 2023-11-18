fd8ed755877a1274e4f7683dd6c9de63c66caa73
Luis Prieto <116104082+LLuisPP@users.noreply.github.com>
2023-11-18 10:23:11 +0100

    Add files via upload

---
+++ get_next_line/get_next_line.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lprieto- <lprieto-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/26 13:12:48 by lprieto-          #+#    #+#             */
+/*   Updated: 2023/11/15 16:10:44 by lprieto-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "get_next_line.h"
+
+char	*ft_read(int fd, char *str)
+{
+	char	*buffer;
+	int	nb_read;
+
+	buffer = ft_malloc(BUFFER_SIZE, 0);
+	nb_read = 1;
+	if (!buffer)
+		return (free(str), str = NULL, NULL);
+	while (nb_read > 0 && !ft_strchr(str, '\n'))
+	{
+		nb_read = read(fd, buffer, BUFFER_SIZE);
+		if (nb_read > 0)
+		{
+			buffer[nb_read] = '\0';
+			str = ft_strjoin(str, buffer);
+		}
+	}
+	free(buffer);
+	if (nb_read < 0)
+		return (free(str), str = NULL, NULL);
+	return (0);
+}
+
+char *get_next_line(int fd)
+{
+	char	*str;
+	char	*line;
+
+	if (fd < 0 || BUFFER_SIZE < 1)
+		return (NULL);
+	str = ft_read(fd, str);
+	if (!str)
+		return (free(str), str = NULL, NULL);
+	str = ft_new_line(str);
+	return (line);
+}
+
+int main (void)
+{
+ int fd = open("punkmanifesto.txt", O_RDONLY);
+ char *line;
+ while ((line = get_next_line(fd)) != NULL)
+     printf("%s", line);
+ return (0);
+}
