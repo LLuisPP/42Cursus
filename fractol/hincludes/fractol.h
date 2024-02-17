1bfa1c7c4641d15e0e838b9ba0f89bd4ef9dfa0a
Luis Prieto <116104082+LLuisPP@users.noreply.github.com>
2024-02-17 20:40:09 +0100

    fractol update
    just need the ft_atof

--- fractol/hincludes/fractol.h
+++ fractol/hincludes/fractol.h
@@ -6,7 +6,7 @@
 /*   By: lprieto- <lprieto-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/05 08:13:12 by lprieto-          #+#    #+#             */
-/*   Updated: 2024/02/17 12:30:09 by lprieto-         ###   ########.fr       */
+/*   Updated: 2024/02/17 19:10:57 by lprieto-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,8 @@
 # include "ft_printf.h"
 # include "mlx.h"
 
-# define HEIGHT	800
-# define WIDTH	800
+# define HEIGHT	500
+# define WIDTH	500
 
 typedef struct s_fractal
 {
@@ -47,6 +47,7 @@ typedef struct s_fractal
 	double	max_re;
 }	t_f;
 
+int		arg_is_int(int argc, char **argv);
 void	init_fractal(t_f *f);
 t_f		*get_fractals(void);
 t_f		*fr_match(char *str);
@@ -55,10 +56,16 @@ int		mouse(int key, int x, int y, t_f *f);
 int		render(t_f *f);
 void	my_mlx_pixel_put(t_f *f, int x, int y, int color);
 int		err_end(char *msg);
+void	menu_optns(void);
 int		exit_click(void);
 int		mandelbrot(double cr, double ci, t_f *f);
 int		julia(double zx, double zy, t_f *f);
 int		tricorn(double cr, double ci, t_f *f);
 int		picker(double re, double im, t_f *f);
+void    print_display(t_f *f);
+
+void	tricorn_ascii(void);
+void	mandelbrot_ascii(void);
+void	julia_ascii(void);
 
 #endif
