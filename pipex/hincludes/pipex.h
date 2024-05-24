9223b9cbf18905ed1acf0702efcd706e7b3554d9
Luis Maria Prieto Pedregal <lprieto-@car13s6.42barcelona.com>
2024-05-24 11:03:41 +0200

    pipex update

--- pipex/hincludes/pipex.h
+++ pipex/hincludes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: lprieto- <lprieto-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/19 21:23:36 by lprieto-          #+#    #+#             */
-/*   Updated: 2024/02/20 11:34:44 by lprieto-         ###   ########.fr       */
+/*   Updated: 2024/05/24 10:29:05 by lprieto-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,32 @@
 # include "libft.h"
 # include "ft_printf.h"
 
+/* * * * * * * * *   Error macros * * * * * * * * * */
+
+# define E_ARG "Error: Invalid number of parameters\n"
+# define E_MALLOC "Error: Malloc failure\n"
+# define E_ENV "Error: No environment\n"
+# define E_PATH "Error: No such path\n"
+# define E_CMD "Error: Command not found\n"
+# define E_OPEN "Error: Can't open file\n"
+# define E_WR "Error: Write permission denied\n"
+# define E_RD "Error: Read permission denied\n"
+# define E_NOFILE "Error: No such file or directory\n"
+# define E_DIRECTORY "Error: Is a directory\n"
+
+/* structs */
+
 typedef struct s_pipe
 {
-	char	*name;
+	int		argc;
+	char	**argv;
+	char	**env;
+	char	**path;
+
 }	t_p;
 
-int	err_end(char *msg);
+/* pipex functions */
+
+void	err_end(char *msg);
 
 #endif
--- pipex/src/pipex.c
+++ pipex/src/pipex.c
@@ -6,15 +6,36 @@
 /*   By: lprieto- <lprieto-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 12:37:09 by lprieto-          #+#    #+#             */
-/*   Updated: 2024/02/19 21:34:13 by lprieto-         ###   ########.fr       */
+/*   Updated: 2024/05/24 10:47:01 by lprieto-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+#include <unistd.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
 
-int	main(int argc, char **argv)
+int	main(int argc, char **argv, char **env)
 {
-	if (argc == 2)
-		ft_printf("argument: %s\n", argv[1]);
+	t_p	*pipe;
+	int fd;
+
+	fd = open("1", O_RDONLY);
+	perror("hola frank:");
+return (0);
+	if (argc != 5)
+		err_end(E_ARG);
+	ft_bzero(&pipe, sizeof(t_p));
+	pipe = (t_p *)malloc(sizeof(t_p));
+	if(!pipe){
+		perror(E_MALLOC);
+		exit(0);
+	}
+
+	ft_printf("\n", env);
+	ft_printf("\n", argv[0]);
+	ft_printf("que hay en pipe?\n", pipe);	
 	return (0);
 }
