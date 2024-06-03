18273fc5f14df829272b3487317ece8affd56dba
lprieto- <lprieto-@lprieto-U22.04vm>
2024-06-03 19:46:03 +0200

    Linux philo migrate

---
+++ philosophers/hincludes/philo.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lprieto- <lprieto-@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/19 21:23:36 by lprieto-          #+#    #+#             */
+/*   Updated: 2024/05/30 07:47:42 by lprieto-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <limits.h>
+# include "libft.h"
+# include "ft_printf.h"
+
+/* Structs */
+
+
+/* Philo functions */
+
+
+#endif
