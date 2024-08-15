eb1afac8ede18ab8cd51bb4fb12ed08a7d685834
Luis María Prieto Pedregal <lprieto-@car10s6.42barcelona.com>
2024-08-15 12:22:23 +0200

    1 philo gestion

--- philosophers/src/ph_routine.c
+++ philosophers/src/ph_routine.c
@@ -6,7 +6,7 @@
 /*   By: lprieto- <lprieto-@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/11 18:23:01 by lprieto-          #+#    #+#             */
-/*   Updated: 2024/08/15 08:16:32 by lprieto-         ###   ########.fr       */
+/*   Updated: 2024/08/15 12:20:39 by lprieto-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,16 @@ void	pick_up_forks(t_table *table, int id, int l_fork, int r_fork)
 	if (table->feast_end != 1)
 		print_status(table, id, TF, PK);
 	pthread_mutex_unlock(&table->data_m);
+	pthread_mutex_unlock(&table->data_m);
+	if (&table->forks[l_fork] == &table->forks[r_fork])
+	{
+		usleep(table->t_to_die * 1000);
+		pthread_mutex_unlock(&table->forks[l_fork]);
+		print_status(table, id, DI, RD);
+		table->feast_end = 1;
+		pthread_mutex_unlock(&table->data_m);
+		return ;
+	}
 	pthread_mutex_lock(&table->forks[r_fork]);
 	pthread_mutex_lock(&table->data_m);
 	if (table->feast_end != 1)
