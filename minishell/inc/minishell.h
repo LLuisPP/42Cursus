e3a961b0c0d39416c4b4efdcc4250b4ab936cdc9
Luis Prieto <116104082+LLuisPP@users.noreply.github.com>
2024-08-19 10:50:33 +0200

    ms update

---
+++ minishell/inc/minishell.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lprieto- <lprieto-@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/19 09:26:23 by lprieto-          #+#    #+#             */
+/*   Updated: 2024/08/19 09:29:47 by lprieto-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "libft.h"
+# include "readline/readline.h"
+# include "readline/history.h"
+# include <stddef.h>
+# include <stdio.h>
+# include <limits.h>
+# include <dirent.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+# include <sys/types.h>
+# include <fcntl.h>
+# include <errno.h>
+# include <signal.h>
+# include <string.h>
+
+#endif
