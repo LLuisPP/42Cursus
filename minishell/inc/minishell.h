f065dc0331f442d3203a207db31e0854f98ed79e
Luis María Prieto Pedregal <lprieto-@cbr5s3.42barcelona.com>
2024-09-12 18:22:22 +0200

    msh update

--- minishell/inc/minishell.h
+++ minishell/inc/minishell.h
@@ -6,7 +6,7 @@
 /*   By: lprieto- <lprieto-@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/19 09:26:23 by lprieto-          #+#    #+#             */
-/*   Updated: 2024/08/20 12:46:28 by lprieto-         ###   ########.fr       */
+/*   Updated: 2024/09/09 11:31:49 by lprieto-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,14 +30,72 @@
 # include <signal.h>
 # include <string.h>
 
-typedef struct s_history {
-    char **commands;
-    int count;
-    int capacity;
-} t_hist;
+/******************************** Structs Simplification ******************/
+
+typedef struct s_history t_hist;
+typedef	struct s_env t_env;
+
+/******************************** Structs *********************************/
+
+struct	s_history
+{
+	char	**commands;
+	int		count;
+	int		capacity;
+};
+
+struct	s_env
+{
+	char	*path;
+	char	*pwd;
+	char	*home;
+};
+
+/******************************* Functions ********************************/
 
 char	*command_generator(const char *text, int state);
-char    **command_completion(const char *text, int start, int end);
-char    *file_generator(const char *text, int state);
+char	**command_completion(const char *text, int start, int end);
+char	*file_generator(const char *text, int state);
+char	*parse_path(char **env);
+
+/******************************* Parsing **********************************/
+
+char	*ft_strtok(char *str, const char *separator);
+char	*parse_pwd(char **env);
+
+/******************************* Tokenizer ********************************/
+
+/******************************* Initialize *******************************/
+
+int	init_structs(t_env *env, t_hist *hist);
+
+/******************************* Executor *********************************/
+
+
+/******************************* Error macros *****************************/
+
+# define E_ARG "Invalid number of parameters\n"
+# define E_MALLOC "Malloc failure\n"
+# define E_ENV "Error: No environment\n"
+# define E_PATH "No such path\n"
+# define E_CMD "Error: Command not found\n"
+# define E_OPEN "Can't open file\n"
+# define E_WR "W Permission denied\n"
+# define E_RD "R Permission denied\n"
+# define E_X "X Permission denied\n"
+# define E_NOFILE "No such file or directory\n"
+# define E_DIRECTORY "Is a directory\n"
+# define E_EXECARG "Error: minishell doesn't accept arguments\n"
+
+/******************************** Color macros ***************************/
+
+# define RD		"\033[1;31m"
+# define GR		"\033[1;32m"
+# define YL		"\033[1;33m"
+# define BL		"\033[1;34m"
+# define PK		"\033[0;35m"
+# define PP		"\033[1;35m"
+# define CY		"\033[1;36m"
+# define F		"\033[0m"
 
 #endif
--- minishell/src/minishell.c
+++ minishell/src/minishell.c
@@ -6,7 +6,7 @@
 /*   By: lprieto- <lprieto-@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/19 09:25:04 by lprieto-          #+#    #+#             */
-/*   Updated: 2024/09/03 11:06:20 by lprieto-         ###   ########.fr       */
+/*   Updated: 2024/09/09 11:32:15 by lprieto-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,9 +28,7 @@ char	*command_generator(const char *text, int state)
 	while ((name = command_list[list_index++]))
 	{
 		if (ft_strncmp(name, text, len) == 0)
-		{
 			return (ft_strdup(name));
-		}
 	}
     return (NULL);
 }
@@ -40,70 +38,127 @@ char **command_completion(const char *text, int start, int end)
     char **matches = NULL;
 
     if (start != 0 && end > start)
-	{
         matches = rl_completion_matches(text, file_generator);
-    }
 	else
-	{
         matches = rl_completion_matches(text, command_generator);
-    }
 
     return (matches);
 }
 
 char	*file_generator(const char *text, int state)
 {
-    static DIR *dir;
-    static struct dirent *entry;
-    int len;
+    static DIR				*dir;
+    static struct dirent	*entry;
+    int						len;
 	
 	len = ft_strlen(text);
 
-    if (!state) {
-        dir = opendir(".");
-    }
+    if (!state)
+		dir = opendir(".");
 
-    if (dir) {
+    if (dir)
+	{
         while ((entry = readdir(dir))) {
             if (ft_strncmp(entry->d_name, text, len) == 0)
-			{
                 return (ft_strdup(entry->d_name));
-            }
         }
         closedir (dir);
         dir = NULL;
     }
-
     return (NULL);
 }
+/* Esta funcion es para trocear un path del env (no estoy trabajando en ella, se puede modificar)*/
+char	*parse_path(char **envs)
+{
+	int		i;
+	char	**path;
 
+	i = 0;
+	path = NULL;
+	while (envs[i] && !path)
+	{
+		if (ft_strncmp(envs[i], "PATH=", 5) == 0)
+		{
+			path = ft_split(&envs[i][5], ';');
+			if (!path)
+				exit (-1);
+		}
+		i++;
+	}
+	if (!path)
+		exit (-1);
+	return (*path);
+}
+char	*parse_pwd(char **envs)
+{
+	int		i;
+	char	**pwd;
 
-int main(int argc, char **argv, char **env)
+	i = 0;
+	pwd = NULL;
+	while (envs[i] && !pwd)
+	{
+		if (ft_strncmp(envs[i], "PWD=", 4) == 0)
+		{
+			pwd = ft_split(&envs[i][5], ';');
+			if (!pwd)
+				exit (-1);
+		}
+		i++;
+	}
+	if (!pwd)
+		exit (-1);
+	return (*pwd);
+}
+
+char	*ft_strtok(char *str, const char *separator)
+{
+	char *ptr = NULL;
+	char *start = NULL;
+	
+	if (str == NULL)
+		return (NULL);
+	start = ptr;
+	while (*ptr != '\0' && ft_strchr(separator, *ptr) == NULL)
+		ptr++;
+	if (*ptr != '\0')
+		*ptr++ = '\0';
+	return (start);
+}
+
+int	init_structs(t_env *env, t_hist *hist)
+{
+	ft_bzero(env, sizeof(t_env));
+	ft_bzero(hist, sizeof(t_hist));	
+	return (0);
+}
+
+int main(int argc, char **argv, char **envs)
 {
 	char *input;
 	t_hist	hist;
+	t_env	env;
+	//char **tokens = NULL;
+	char	*pwd;
 
-	(void)argc;
-	(void)argv;
-	(void)env;
-	ft_bzero(&hist, sizeof(t_hist));
+	if (argc != 1 || argv[1])
+	{
+		ft_fd_printf(2, "%s", E_EXECARG);
+		exit (0);
+	}
+	init_structs(&env, &hist);
+	pwd = parse_pwd(envs);
+	printf("pwd: %s\n", pwd);
 	while(1)
 	{
 		rl_attempted_completion_function = command_completion;
 		input = readline("\033[1;96m 👽 Space 🛸 shell $\e[0m> ");
 		if (input == NULL)
-		{
 			break;
-		}
-
+		//*tokens = ft_strtok(argv[1], " ");
         if (*input)
-		{
             add_history(input);
-        }
-		//execve("/bin/ls", argv, NULL);
-
-		(void)input;
-
+		execve("/bin/ls", argv, NULL);
         free(input);
     }
 
