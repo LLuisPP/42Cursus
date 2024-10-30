ce0b521915216503e00a3b35abc095c7aded5f6e
Luis María Prieto Pedregal <lprieto-@car8s5.42barcelona.com>
2024-10-30 10:04:18 +0100

    N | norminette OK ms_signals.c

--- minishell/src/ms_signals.c
+++ minishell/src/ms_signals.c
@@ -6,41 +6,40 @@
 /*   By: lprieto- <lprieto-@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 08:13:41 by lprieto-          #+#    #+#             */
-/*   Updated: 2024/10/30 08:28:59 by lprieto-         ###   ########.fr       */
+/*   Updated: 2024/10/30 10:03:57 by lprieto-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 /* para manejar el Ctrl-C y que no se salga del minishell */
-static void handle_sigint(int sig)
+static	void	handle_sigint(int sig)
 {
-    (void)sig;
-    write(1, "\n", 1);
-    rl_on_new_line();
-    rl_replace_line("", 0);
-    rl_redisplay();
+	(void)sig;
+	write(1, "\n", 1);
+	rl_on_new_line();
+	rl_replace_line("", 0);
+	rl_redisplay();
 }
 
-
 /* aqui ignoramos el Ctrl - */
-static void handle_sigquit(int sig)
+static void	handle_sigquit(int sig)
 {
-    (void)sig;
+	(void)sig;
 }
 
 /* aqui el controlador de señales que llama a cualquier uso */
-void    init_signals(void)
+void	init_signals(void)
 {
-    struct sigaction sa_int;
-    struct sigaction sa_quit;
+	struct sigaction	sa_int;
+	struct sigaction	sa_quit;
 
-    sa_int.sa_handler = handle_sigint;
-    sa_quit.sa_handler = handle_sigquit;
-    sigemptyset(&sa_int.sa_mask);
-    sigemptyset(&sa_quit.sa_mask);
-    sa_int.sa_flags = 0;
-    sa_quit.sa_flags = 0;
-    sigaction(SIGINT, &sa_int, NULL);
-    sigaction(SIGQUIT, &sa_quit, NULL);
+	sa_int.sa_handler = handle_sigint;
+	sa_quit.sa_handler = handle_sigquit;
+	sigemptyset(&sa_int.sa_mask);
+	sigemptyset(&sa_quit.sa_mask);
+	sa_int.sa_flags = 0;
+	sa_quit.sa_flags = 0;
+	sigaction(SIGINT, &sa_int, NULL);
+	sigaction(SIGQUIT, &sa_quit, NULL);
 }
