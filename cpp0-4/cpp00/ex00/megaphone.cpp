3d34e817163843cdc2e9e933f02e159333ad56a8
lprieto- <lprieto-@lprieto-U22.04vm>
2025-06-11 13:12:47 +0200

    + > | ex00 megaphone add

---
+++ cpp0-4/cpp00/ex00/megaphone.cpp
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lprieto- <lprieto-@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/11 11:27:21 by lprieto-          #+#    #+#             */
+/*   Updated: 2025/06/11 13:03:02 by lprieto-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    int i;
+    int j;
+
+    if (argc < 2)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *"
+        << std::endl;
+        return (0);
+    }
+    else {
+        i = -1;
+        while (i < argc)
+        {
+            j = 0;
+            while (argc > 1 && argv && argv[++i])
+            {
+                j = -1;
+                while (i > 0 && argv[i][++j])
+                    std::cout << (char)std::toupper(argv[i][j]);
+                    std::cout << " ";
+            }
+        }
+        std::cout << std::endl;
+    }
+    return (0);
+}
