31d844ef20d3db8ca350c6e3ff0de993c26c1933
lprieto- <lprieto-@lprieto-U22.04vm>
2025-10-14 21:34:34 +0200

    > | ex01 main.cpp

--- cpp0-4/cpp00/ex01/src/Main.cpp
+++ cpp0-4/cpp00/ex01/src/Main.cpp
@@ -6,7 +6,7 @@
 /*   By: lprieto- <lprieto-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/14 18:56:11 by lprieto-          #+#    #+#             */
-/*   Updated: 2025/10/14 20:02:01 by lprieto-         ###   ########.fr       */
+/*   Updated: 2025/10/14 21:31:32 by lprieto-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,40 @@ void	print_instructions(void) {
 	std::cout << "\tEXIT" << std::endl;
 }
 
+void	print_search(void) {
+	std::cout << "" << std::endl;
+	std::cout << "|>------------------------------<|" << std::endl << std::endl;
+	std::cout << "⠀    ⠀⠀⠀⢀⣰⣶⣶⣿⣿⣿⣿⣿⣶⣶⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ " << std::endl;
+	std::cout << "    ⠀⠀⢀⣴⣿⡿⠟⠋⠉⠉⠉⠉⠉⠙⠻⣿⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀ " << std::endl;
+	std::cout << "    ⢀⣰⣿⡿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀ " << std::endl;
+	std::cout << "    ⢸⣿⡿⠁⠀⠀Here are⠀⠀⠀⠈⣿⣿⡆⠀⠀⠀⠀⠀⠀⠀ " << std::endl;
+	std::cout << "    ⣿⣿⡇⠀⠀⠀⠀⠀ the⠀⠀⠀⠀⠀⠀⢸⣿⣿⠀⠀⠀⠀⠀⠀⠀ " << std::endl;
+	std::cout << "    ⣿⣿⡇⠀⠀⠀⠀results⠀⠀⠀⠀⢸⣿⣿⠀⠀⠀⠀⠀⠀⠀ " << std::endl;
+	std::cout << "    ⢻⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⡏⠀⠀⠀⠀⠀⠀⠀ " << std::endl;
+	std::cout << "    ⠸⢿⣿⣦⠀⠀⠀⠀ ⠀:)⠀⠀⠀⠀⢀⣼⣿⡿⠁⠀⠀⠀⠀⠀⠀⠀ " << std::endl;
+	std::cout << "    ⠀⠈⠻⣿⣷⣤⡀⠀⠀⠀⠀⠀⠀⠀⣀⣴⣾⣿⣿⡁⠀⠀⠀⠀⠀⠀⠀⠀ " << std::endl;
+	std::cout << "    ⠀⠀⠀⠈⠻⢿⣿⣿⣶⣶⣶⣶⣶⣿⣿⡿⠟⠻⣿⣿⣦⣀⣀⠀⠀⠀⠀⠀ " << std::endl;
+	std::cout << "    ⠀    ⠀⠈⠉⠛⠛⠛⠛⠋⠉⠁⠀⠀⠀⠈⣻⣿⣿⣿⣷⣄⠀⠀⠀ " << std::endl;
+	std::cout << "    ⠀⠀⠀⠀    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⣿⣿⡷   " << std::endl;
+	std::cout << "    ⠀     ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⡿⠋    " << std::endl;
+	std::cout << "" << std::endl;
+	std::cout << "|>  Here are the search resuls  <|" << std::endl;
+	std::cout << "|>------------------------------<|" << std::endl << std::endl;
+}
 
+void	print_add(void) {
+	std::cout << "" << std::endl;
+	std::cout << "|>------------------------------<|" << std::endl << std::endl;
+	std::cout << "      ⠀⠀⠀⠀⣿⣿⣿⣿ ⠀⠀⠀⠀⠀⠀⠀⠀⠀        ⠀ " << std::endl;
+	std::cout << "  ⠀       ⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀Fill the⠀⠀⠀⠀ " << std::endl;
+	std::cout << "  ⠀⠀⠀ ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀  form    ⠀⠀ " << std::endl;
+	std::cout << "  ⠀   ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿ ⠀⠀ to add   ⠀⠀ " << std::endl;
+	std::cout << "  ⠀    ⠀⠀⠀⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀a contact⠀⠀⠀ " << std::endl;
+	std::cout << "  ⠀⠀⠀    ⠀⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀         ⠀ " << std::endl;
+	std::cout << "" << std::endl;
+	std::cout << "|>   Fill in to add a contact   <|" << std::endl;
+	std::cout << "|>------------------------------<|" << std::endl << std::endl;
+}
 
 Contact	make_contact(void)
 {
@@ -54,9 +87,9 @@ int	main(void) {
 		if (input == "EXIT")
 			return (0);
 		else if (input == "ADD")
-			std::cout << "ADD is working" << std::endl;
+			print_add();
 		else if (input == "SEARCH")
-			std::cout << "SEARCH is working" << std::endl;
+			print_search();
 		else
 			std::cout << "Wrong option, try again" << std::endl;
 		print_instructions();
