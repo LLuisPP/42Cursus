5389e094070b564ab155c67be07114e1e25aff4c
lprieto- <lprieto-@lprieto-U22.04vm>
2025-10-22 20:56:45 +0200

    + | replace vocal/string

--- cpp0-4/cpp01/ex04/src/main.cpp
+++ cpp0-4/cpp01/ex04/src/main.cpp
@@ -13,14 +13,48 @@
 #include <iostream>
 #include <fstream>
 
-int	main(int argc, char **argv) {
-
-	if (argc != 4) {
-		std::cout << "3 arguments required !" << std::endl;
-		return (1);
+void	replace(std::ifstream *infile, std::ofstream *outfile, std::string str1, std::string str2)
+{
+	std::string		line;
+	
+	while (std::getline(*infile, line))
+	{
+		for (int i = 0, j; line[i]; i++)
+		{
+			j = 0;
+			while (str1[j])
+			{
+				if (line[i + j] != str1[j])
+					break;
+				j++;
+			}
+			if (j == (int) str1.length() && j)
+			{
+				*outfile << str2;
+				i += j - 1;
+			}
+			else
+				*outfile << line[i];
+		}
+		*outfile << '\n';
 	}
-	else
-		std::cout << "testing" << std::endl;
-	(void*)argv;
+}
+
+int	main(int argc, char **argv)
+{
+	std::string	filename;
+
+	if (argc != 4)
+		return (std::cout << "3 Args required" << std::endl, 1);
+	filename = (std::string) argv[1] + ".replace";
+	std::ifstream	infile(argv[1]);
+	if (!infile.is_open())
+		return (std::cout << "Can't open " << argv[1] << " file" << std::endl, 1);
+	std::ofstream	outfile(filename.c_str());
+	if (!infile.is_open())
+		return (std::cout << "Can't open " << argv[1] << ".replace " << " file" << std::endl, 1);
+	replace(&infile, &outfile, argv[2], argv[3]);
+	infile.close();
+	outfile.close();
 	return (0);
 }
