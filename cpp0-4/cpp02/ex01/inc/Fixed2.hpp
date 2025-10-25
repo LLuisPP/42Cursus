bc9ddf5ff2983c505e19fc4efff4aae50c4d0b36
lprieto- <lprieto-@lprieto-U22.04vm>
2025-10-25 12:14:46 +0200

    + | ex01 almost working

---
+++ cpp0-4/cpp02/ex01/inc/Fixed2.hpp
@@ -0,0 +1,64 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed2.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lprieto- <lprieto-@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/25 09:59:15 by lprieto-          #+#    #+#             */
+/*   Updated: 2025/10/25 11:23:40 by lprieto-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED2_HPP
+# define FIXED2_HPP
+
+// Color macros:
+#define RES	"\033[0m"
+#define BGY	"\033[1;90m"
+#define BR	"\033[1;91m"
+#define BGR	"\033[1;92m"
+#define BY	"\033[1;33m"
+#define BB	"\033[1;94m"
+#define BC	"\033[1;96m"
+#define BW	"\033[1;97m"
+#define GY	"\033[0;90m"
+#define R	"\033[0;91m"
+#define G	"\033[0;92m"
+#define Y	"\033[0;93m"
+#define B	"\033[0;94m"
+#define C	"\033[0;96m"
+#define W	"\033[0;97m"
+#define PP	"\033[0;95m"
+#define BD	"\033[1m"
+#define OR	"\033[38;5;214m"
+// macros
+
+#include <iostream>
+#include <ostream>
+#include <cmath>
+
+class	Fixed {
+
+	private:
+		int					_rawBits;
+		static const int	_fractionalBits;
+
+	public:
+		Fixed();
+		Fixed (Fixed const &copy);
+		Fixed& operator = (const Fixed &copy);
+		~Fixed();
+
+		Fixed(int n);
+		Fixed(float f);
+		float	toFloat(void) const;
+		int		toInt(void) const;
+
+		int		getRawBits(void) const;
+		void	setRawBits(int const raw);
+};
+
+std::ostream &operator << (std::ostream &outstr, Fixed const &fix);
+
+#endif
--- cpp0-4/cpp02/ex01/src/main.cpp
+++ cpp0-4/cpp02/ex01/src/main.cpp
@@ -6,8 +6,28 @@
 /*   By: lprieto- <lprieto-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/24 20:21:37 by lprieto-          #+#    #+#             */
-/*   Updated: 2025/10/24 20:21:41 by lprieto-         ###   ########.fr       */
+/*   Updated: 2025/10/25 11:24:05 by lprieto-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Fixed2.hpp"
 
+int main( void ) {
+
+    Fixed a;
+    Fixed const b( 10 );
+	Fixed const c( 42.42f );
+	Fixed const d( b );
+    a = Fixed( 1234.4321f );
+
+    std::cout << "a is " << a << std::endl;
+    std::cout << "b is " << b << std::endl;
+    std::cout << "c is " << c << std::endl;
+    std::cout << "d is " << d << std::endl;
+    std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+    std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+    std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+    std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+
+    return (0);
+}
