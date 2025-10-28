c135d02d897f5cf886535ef954891d6563f6dc1f
lprieto- <lprieto-@lprieto-U22.04vm>
2025-10-28 10:33:44 +0100

    + | ex00 working

---
+++ cpp0-4/cpp03/ex00/src/main.cpp
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lprieto- <lprieto-@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/28 08:31:15 by lprieto-          #+#    #+#             */
+/*   Updated: 2025/10/28 09:04:11 by lprieto-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+int	main(void) {
+
+	ClapTrap	clap("Bonny");
+	ClapTrap	trap("Clyde");
+
+
+	clap.beRepaired(6);
+	trap.beRepaired(7);
+	
+	clap.attack("Clyde");
+	trap.takeDamage(5);
+	
+	trap.attack("Bonny");
+	clap.takeDamage(5);
+	
+	clap.attack("Clyde");
+	trap.takeDamage(5);
+	
+	trap.attack("Bonny");
+	clap.takeDamage(5);
+
+	clap.attack("Clyde");
+	trap.takeDamage(5);
+	
+	trap.attack("Bonny");
+	clap.takeDamage(5);
+
+	
+	clap.attack("Clyde");
+	trap.takeDamage(50);
+	trap.attack("Bonny");
+
+	clap.takeDamage(5);
+	
+	clap.attack("Clyde");
+	trap.attack("Bonny");
+
+	return (0);
+}
