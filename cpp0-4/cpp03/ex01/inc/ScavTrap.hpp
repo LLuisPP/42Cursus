a305d9bcc09b75ac72a5aae505a4c129d6e8edd8
lprieto- <lprieto-@lprieto-U22.04vm>
2025-10-29 11:36:28 +0100

    > | local features, split Clap left

--- cpp0-4/cpp03/ex01/inc/ScavTrap.hpp
+++ cpp0-4/cpp03/ex01/inc/ScavTrap.hpp
@@ -6,13 +6,26 @@
 /*   By: lprieto- <lprieto-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/28 11:02:38 by lprieto-          #+#    #+#             */
-/*   Updated: 2025/10/28 11:03:25 by lprieto-         ###   ########.fr       */
+/*   Updated: 2025/10/29 10:08:08 by lprieto-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SCAVTRAP_HPP
 # define SCAVTRAP_HPP
 
+class	ScavTrap : public ClapTrap {
 
+	private:
+		bool	_guard;
+	public:
+		scavTrap(void);
+		ScavTrap(const std::string name);
+		ScavTrap(const ScavTrap &copy);
+		ScavTrap &operator=(const ScavTrap &copy);
+		~ScavTrap(void);
+
+		void	attack(const std::string &target);
+		void	guardGate(void);
+};
 
 #endif
--- cpp0-4/cpp03/ex01/src/ScavTrap.cpp
+++ cpp0-4/cpp03/ex01/src/ScavTrap.cpp
@@ -6,9 +6,68 @@
 /*   By: lprieto- <lprieto-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/28 11:03:42 by lprieto-          #+#    #+#             */
-/*   Updated: 2025/10/28 11:04:15 by lprieto-         ###   ########.fr       */
+/*   Updated: 2025/10/29 10:25:35 by lprieto-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScavTrap.hpp"
-#include <iostream>
+
+ScavTrap::ScavTrap(void) ClapTrap(), _guard(false) {
+
+	_health = 100;
+	_energy = 50;
+	_attack = 20;
+	std::cout << "ScavTrap created" << std::endl;
+}
+
+ScavTrap::ScavTrap(std::string name): ClapTrap(name), _guard(false) {
+
+	_health = 100;
+	_energy = 50;
+	_attack = 20;
+	std::cout << "ScavTrap " << _name << " created" << std::endl;
+}
+
+
+ScavTrap::ScavTrap(const ScavTrap &copy): ClapTrap(copy), _guard(false) {
+
+	*this = copy;
+	std::cout << "ScavTrap " << _name << " copy created" << std::endl;
+}
+
+ScavTrap	&ScavTrap::operator=(const ScavTrap &copy) {
+
+	_name = copy._name;
+	_health = copy._health;
+	_energy = copy._energy;
+	_attack = copy._attack;
+	std::cout << "ScavTrap " << _name << " copy assigned" << std::endl;
+	return (*this);
+}
+
+ScavTrap::~ScavTrap(void) {
+
+	std::cout << "ScavTrap " << _name << " destroyed !!" << std::endl;	
+}
+
+void	ScavTrap::attack(const std::string &target) {
+
+	if (_energy > 0 && _health > 0 && !_guard) {
+		_energy--;
+		std::cout << "ScavTrap " << _name << " attacks " << target << ", causing" << _attack << " points of damage" << std::endl;	
+	}
+	else
+		std::cout << "ScavTrap " << _name << " can't attack!" << std::endl;	
+}
+
+void	ScavTrap::guardGate(void) {
+
+	_guard = !_guard;
+	if (_guard)
+		std::cout << "ScavTrap " << _name << " is in guard" << std::endl;	
+	else
+		std::cout << "ScavTrap " << _name << " leaves the guard" << std::endl;	
+}
+
+
+
